f2db340e2deb78e9895c08bd4c1c3921fd0190f7
Kenny Harrer <kenny.harrer@icloud.com>
2024-01-08 14:04:17 -0600

    remove auth middleware from get

--- gluapacker/api/index.js
+++ gluapacker/api/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.use('/addcsluafiles', authMiddleware, require('./addCsLuaFile.js'))
 router.use('/clearcache', authMiddleware, require('./clearCache.js'))
-router.use('/getcsluafiles', authMiddleware, require('./clearCache.js'))
+router.use('/getcsluafiles', require('./clearCache.js'))
 
 
 
