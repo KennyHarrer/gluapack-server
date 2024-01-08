a523cc9e52d9f4f61ae19a6a790a8abafe6ecc29
Kenny Harrer <kenny.harrer@icloud.com>
2024-01-08 14:13:06 -0600

    getcsluafile should getcsluafile lol

--- gluapacker/api/index.js
+++ gluapacker/api/index.js
@@ -1,13 +1,11 @@
-const authMiddleware = require('../authorizationMiddleware.js')
+const authMiddleware = require('../authorizationMiddleware.js');
 
-const express = require('express')
+const express = require('express');
 
 const router = express.Router();
 
-router.use('/addcsluafiles', authMiddleware, require('./addCsLuaFile.js'))
-router.use('/clearcache', authMiddleware, require('./clearCache.js'))
-router.use('/getcsluafiles', require('./clearCache.js'))
+router.use('/addcsluafiles', authMiddleware, require('./addCsLuaFile.js'));
+router.use('/clearcache', authMiddleware, require('./clearCache.js'));
+router.use('/getcsluafiles', require('./getCsLua.js'));
 
-
-
-module.exports = router
+module.exports = router;
